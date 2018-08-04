a430d8d6b47ad1c7382dd4a38278613fa99f0b89
Alicia Yu <alicia.yu.16@gmail.com>
2018-08-04 01:56:29 +0000

    automatically backed up by learn

--- latest_movies/lib/latest_movies/cli.rb
+++ latest_movies/lib/latest_movies/cli.rb
@@ -15,6 +15,7 @@ class LatestMovies::CLI
     while input != "exit"
       puts "Please enter the number of the movie you would like more information on or type list to view the list of movies or type exit:"
       input = gets.strip.downcase
+
       if input.to_i > 0
         puts @movies[input.to_i - 1]
       elsif input == "list"
@@ -22,6 +23,7 @@ class LatestMovies::CLI
       else
         puts "Not sure what you want, type list or exit"
       end
+
     end
   end
 
